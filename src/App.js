0256a08297dc5881e16885089a9360eb36805ed9
Pratik Manta <pratik.manta10@gmail.com>
2019-12-10 05:02:51 +0530

    added navbar with button

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>To Do App</h1>
+      <div className="m-4 p-4">
+        <nav class="navbar navbar-light text-warning border-bottom ">          
+            <h1 className="text-center">To Do</h1>
+            <span style={styles.buttonStyle}>
+              <i class="fa fa-plus-square"></i>
+            </span>
+        </nav>  
       </div>
     );
+  }
+}
+
+const styles = {
+  buttonStyle : {
+    fontSize: '4em',
+    cursor:'pointer'
   }
 }
 
