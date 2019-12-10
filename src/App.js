a47afaab8b959ef7ae3026335e279d87a99dca01
Pratik Manta <pratik.manta10@gmail.com>
2019-12-10 16:26:14 +0530

    added input modal to add tasks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
 import React, { Component } from 'react';
+import Modal from 'react-responsive-modal';
+
 
 class App extends Component {
+  state = {
+    isOpen: false,
+    task: ''
+  };
+
+  toggleModal = () => {
+    this.setState(prevState => ({isOpen: !prevState.isOpen}));
+  };
+
+  handleInput = (e) => {
+    console.log(e.target.value)
+  }
+
+  renderModal = () => {
+    return (
+      <Modal 
+        open={this.state.isOpen} 
+        showCloseIcon={false} center
+        onClose={this.toggleModal}
+        styles={{
+          modal: styles.modalStyle
+        }}
+      >
+        <div style={styles.inputBox}>
+          <h3 className="text-center">To Do</h3>
+          <div className="form-group">
+            <textarea className="form-control" rows="3" onChange={this.handleInput}/>
+          </div>
+          <button className="btn btn-block btn-warning">Add to list</button>
+        </div>
+      </Modal>
+    )
+  }
+   
+
   render() {
     return (
       <div className="m-4 p-4">
-        <nav class="navbar navbar-light text-warning border-bottom ">          
-            <h1 className="text-center">To Do</h1>
-            <span style={styles.buttonStyle}>
-              <i class="fa fa-plus-square"></i>
+        <nav className="navbar navbar-light text-warning border-bottom ">          
+            <h1>To Do</h1>
+            <span style={styles.buttonStyle} onClick={this.toggleModal}>
+              <i className="fa fa-plus-square"></i>
             </span>
-        </nav>  
+        </nav> 
+        {this.renderModal()}
       </div>
     );
   }
 }
 
 const styles = {
+
+  inputBox : {
+    width: '400px',
+  },
   buttonStyle : {
     fontSize: '4em',
-    cursor:'pointer'
+    cursor:'pointer',
+  },
+  modalStyle : {
+    display: 'flex',
+    justifyContent: 'center',
   }
 }
 
