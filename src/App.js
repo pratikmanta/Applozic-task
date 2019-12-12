b285c927d7ff395d655ef14dcdecb2ab5cc93f9d
Pratik Manta <pratik.manta10@gmail.com>
2019-12-12 05:57:33 +0530

    removed modal and added task-item component

--- src/App.js
+++ src/App.js
@@ -1,70 +1,100 @@
 import React, { Component } from 'react';
-import Modal from 'react-responsive-modal';
+import TaskItem from './taskItem/TaskItem';
 
 
 class App extends Component {
-  state = {
-    isOpen: false,
-    task: ''
-  };
-
-  toggleModal = () => {
-    this.setState(prevState => ({isOpen: !prevState.isOpen}));
-  };
-
-  handleInput = (e) => {
-    console.log(e.target.value)
+  constructor() {
+    super()
+    this.state = {
+      isOpen: false,
+      value: '',
+      tasklist: []
+    }
   }
 
-  renderModal = () => {
-    return (
-      <Modal 
-        open={this.state.isOpen} 
-        showCloseIcon={false} center
-        onClose={this.toggleModal}
-        styles={{
-          modal: styles.modalStyle
-        }}
-      >
-        <div style={styles.inputBox}>
-          <h3 className="text-center">To Do</h3>
-          <div className="form-group">
-            <textarea className="form-control" rows="3" onChange={this.handleInput}/>
-          </div>
-          <button className="btn btn-block btn-warning">Add to list</button>
-        </div>
-      </Modal>
-    )
+  componentDidMount() {
+    const list = localStorage.getItem('tasklist');
+    const parsedList = JSON.parse(list);
+    if(list == null || list == undefined){
+      return false
+    }
+    else {
+      this.setState({tasklist: parsedList})
+    }
   }
-   
+
+  handleInputChange = (e) => {
+    this.setState({ value: e.target.value });
+  }
+
+  addTask = () => {
+    var listArray = this.state.tasklist;
+    
+    listArray.push({
+      id: listArray.length,
+      content:this.state.value
+    });
+
+    this.setState({
+      tasklist: listArray,
+      value: this.state.value
+    }, () => {
+        localStorage.setItem('tasklist', JSON.stringify(this.state.tasklist));
+    });
+    this.toggleInputBox();
+  }
+
+  toggleInputBox = () => {
+    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+  }
+
+  handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      this.addTask();
+    }
+  }
 
   render() {
+    const { tasklist } = this.state;
+    
     return (
       <div className="m-4 p-4">
-        <nav className="navbar navbar-light text-warning border-bottom ">          
-            <h1>To Do</h1>
-            <span style={styles.buttonStyle} onClick={this.toggleModal}>
-              <i className="fa fa-plus-square"></i>
-            </span>
-        </nav> 
-        {this.renderModal()}
+        <nav className="navbar navbar-light text-warning border-bottom ">
+          <h1>To Do</h1>
+          <span style={styles.buttonStyle} onClick={this.toggleInputBox}>
+            <i className="fa fa-plus-square"></i>
+          </span>
+        </nav>
+        <section>
+          <ul>
+            {
+              tasklist.map((task, index) => {
+                return (
+                  <li key={index} className="row text-warning m-2 p-3 border-bottom">
+                    <TaskItem task={task} handleEnter={this.handleKeyPress} />
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </section>
+        {this.state.isOpen ?
+          <div className="m-2 p-3">
+            <div className="form-group ml-4">
+              <input className="form-control" onChange={this.handleInputChange} onKeyPress={this.handleKeyPress} autoFocus />
+            </div>
+          </div>
+          : null
+        }
       </div>
     );
   }
 }
 
 const styles = {
-
-  inputBox : {
-    width: '400px',
-  },
-  buttonStyle : {
+  buttonStyle: {
     fontSize: '4em',
-    cursor:'pointer',
-  },
-  modalStyle : {
-    display: 'flex',
-    justifyContent: 'center',
+    cursor: 'pointer',
   }
 }
 
